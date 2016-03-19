efbfb06bd85704f911e99d76f828985159d6ba6d
jk_km@ukr.net <jk_km@ukr.net>
2016-03-19 17:04:21 +0100

    rest service tilbage

--- client/app/admin/therapist/therapist.service.js
+++ client/app/admin/therapist/therapist.service.js
@@ -2,7 +2,7 @@
 
 angular.module('feetClinicApp')
   .factory('TherapistService',function($resource){
-    return $resource('http://feetclinicrest.azurewebsites.net/api/therapists/:id',
+    return $resource('/api/therapist/:id',
       {
         id: '@id'
       }, {
